47baedfddeb368b6bbc0b4f46b0cb72bbda83654
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-10 23:41:46 +0900

    [add]ストロングパラメーターを設定
    セキュリティのため

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,4 +11,12 @@ class CompaniesController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def company_params
+    params.require(:company).
+      permit(:name, :introduction, :address, :mail, :number_of_employees, :capital,
+         :date_of_establishment, :corporate_site)
+  end
 end
