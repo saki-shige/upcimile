3cbb587fcd6db774d1d3955cc03ee59ff5ed1831
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-10 21:44:15 +0900

    companiesコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/show'
+  get 'companies/new'
+  get 'companies/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
