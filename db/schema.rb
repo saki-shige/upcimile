11a315ebcc9ec73a26af0a3710e44146df972bc3
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-11 20:12:59 +0900

    [add]Tagモデル、テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_11_082403) do
+ActiveRecord::Schema.define(version: 2022_07_11_111102) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 2022_07_11_082403) do
     t.date "available_from", null: false
     t.date "available_to"
     t.boolean "can_be_provided", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
