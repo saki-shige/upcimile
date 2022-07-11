53205466563d5341e964d84c949ae64c1bec78d1
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-11 15:16:05 +0900

    [add]Company プロフィールedit,index,show機能の実装

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,27 @@
 class CompaniesController < ApplicationController
   def index
+    @companies = Company.all
   end
 
   def show
-  end
-
-  def new
-    @company = Company.new
+    @company = Company.find(current_company.id)
   end
 
   def edit
+    @company = Company.find(current_company.id)
+  end
+
+  def update
+    @company = Company.find(current_company.id)
+    @company.update!(company_params)
+    redirect_to companies_url
   end
 
   private
 
   def company_params
     params.require(:company).
-      permit(:name, :introduction, :address, :mail, :number_of_employees, :capital,
+      permit(:name, :introduction, :address, :email, :number_of_employees, :capital,
          :date_of_establishment, :corporate_site)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :companies
-  resources :companies
+  resources :companies, only: [:index, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
