362367e9810bf3df22a43f6ac8d57885a488f50b
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-11 22:44:57 +0900

    [add]product登録機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save!
+    redirect_to products_url
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).
+      permit(:name, :introduction, :available_from, :available_to, :can_be_provided, :company_id, :category_id)
   end
 end
